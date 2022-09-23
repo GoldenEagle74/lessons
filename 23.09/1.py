690acba5ace57be68841d6413202eab84f65b44e
Михаил <mshchannikov@yandex.ru>
2022-09-23 12:08:37 +0300

    Добавил 23.09

---
+++ 23.09/1.py
@@ -0,0 +1,7 @@
+n = []
+while True:
+	a = input()
+	if a == '0': break
+	if a in n: print('Эта игра уже записана')
+	else: n.append(a)
+n.sort()
---
+++ 23.09/5.py
@@ -0,0 +1,3 @@
+a = input().split()
+if a == sorted(a): print('Да')
+else: print('Нет')
