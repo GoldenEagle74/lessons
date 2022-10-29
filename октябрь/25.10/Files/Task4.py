b279abfd4dc1b5e64ca96bdaf624bf90aa2d7606
GoldenEagle74 <mshchannikov@yandex.ru>
2022-10-30 01:00:25 +0300

    Исправил Files/Task4

--- октябрь/25.10/Files/Task4.py
+++ октябрь/25.10/Files/Task4.py
@@ -9,6 +9,6 @@ with open(input(),'r') as f:
 	d = {}
 	for line in f.readlines():
 		for word in (''.join(([(lambda x: x if x.isalnum() else ' ')(x) for x in line])).split()):
-			if word in d: d[word] += 1
-			else: d[word] = 1
+			if word.lower() in d: d[word.lower()] += 1
+			else: d[word.lower()] = 1
 	print(' '.join(([k for k, v in d.items() if v == max(d.values())])))
